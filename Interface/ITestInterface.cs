01f3a69c15bff0fe416451102efa37a8457a88b1
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-26 20:46:11 +0530

    Add project files.

---
+++ Interface/ITestInterface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal interface ITestInterface
+    {
+        void Add(int a, int b);
+        
+    }
+    interface ITestInterface2 : ITestInterface
+    {
+        void Sub(int a, int b);
+    }
+
+    class Implementationclass : ITestInterface2
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        static void Main(string[] args)
+        {
+            Implementationclass obj = new Implementationclass();
+            ITestInterface2 i = obj;
+
+            i.Add(100, 30);
+            i.Sub(78, 23);
+
+            // obj.Add(100, 30);
+            // obj.Sub(78, 23);
+            Console.ReadLine();
+        }
+        }
+}
---
+++ Interface/MultipleInheritenceTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface Interface1
+    {
+        void Test();
+        void Show();
+
+    }
+
+    interface Interface2
+    {
+        void Test();
+        void Show();
+    }
+    internal class MultipleInheritenceTest :Interface1, Interface2
+    {
+        public void Test()
+        {
+            Console.WriteLine("Interfaces method  implemented in child class ");
+        }
+        void Interface1.Show()
+        {
+            Console.WriteLine("Declared in interface 1 and implemented in class ");
+        }
+
+        void Interface2.Show()
+        {
+            Console.WriteLine("Declared in interface 2 and implemented in class ");
+        }
+        static void Main(string[] args)
+        {
+            MultipleInheritenceTest obj = new MultipleInheritenceTest();
+            obj.Test();
+            Interface1 i1 = obj;
+            i1.Show();
+            Interface2 i2 = obj;
+            i2.Show();
+            Console.ReadLine();
+        }
+    }
+}
